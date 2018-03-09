e235b9f298ad3f5a1d8864a8ba24090555cdfa0e
Vinayak <vinayak.g@outlook.com>
2018-03-09 13:42:00 +0530

    fixed the course org filter errors

--- cms/djangoapps/contentstore/views/course.py
+++ cms/djangoapps/contentstore/views/course.py
@@ -483,8 +483,13 @@ def course_listing(request):
     else:
         org = None
         show_libraries = LIBRARIES_ENABLED
-    edvayInstances = EdvayInstance.objects.get(user=request.user) 
-    org = edvayInstances.org_name
+    try:
+        edvayInstances = EdvayInstance.objects.get(user=request.user) 
+        org = edvayInstances.org_name
+    except EdvayInstance.DoesNotExist:
+        #do nothing. the user does not belong to the indus platform
+        pass
+              
     courses_iter, in_process_course_actions = get_courses_accessible_to_user(request, org)
     user = request.user
     libraries = _accessible_libraries_iter(request.user) if show_libraries else []
